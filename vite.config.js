f4d250fff0d4924f33bf9f9bf44a6b095f8486c2
caner <canerturkmen@msn.com>
2021-09-14 00:38:46 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'todo-app',
   build: {
     lib: {
       entry: 'src/todo-app/todo-app.js',
